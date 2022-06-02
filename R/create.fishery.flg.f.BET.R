9a8aeb523c07365a36d165f597295c08f4cd2b45
Haikun Xu <haikunxu1990@gmail.com>
2022-06-02 10:56:39 -0700

    test html format

--- R/create.fishery.flg.f.BET.R
+++ R/create.fishery.flg.f.BET.R
@@ -19,23 +19,23 @@ create.fishery.flg.f.BET = function(strat.defns,PS)
   print("Using fishery stratification: Juan/Haikun BET spatial movement model 10S 10N")
   
   areagear<-rep(NA,nrecs)
-  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area>=1 & strat.defns$area<=2]<-"FO.A1"
-  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area>=3 & strat.defns$area<=4]<-"FO.A2"
-  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==5]<-"FO.A3"
-  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==6]<-"FO.A4"
-  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==7]<-"FO.A5"
+  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==1]<-"FO.A1"
+  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==2]<-"FO.A2"
+  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==3]<-"FO.A3"
+  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==4]<-"FO.A4"
+  areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==5]<-"FO.A5"
 
-  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area>=1 & strat.defns$area<=2]<-"UN.A1"
-  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area>=3 & strat.defns$area<=4]<-"UN.A2"
-  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==5]<-"UN.A3"
-  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==6]<-"UN.A4"
-  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==7]<-"UN.A5"
+  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==1]<-"UN.A1"
+  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==2]<-"UN.A2"
+  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==3]<-"UN.A3"
+  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==4]<-"UN.A4"
+  areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==5]<-"UN.A5"
 
-  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area>=1 & strat.defns$area<=2]<-"DP.A1"
-  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area>=3 & strat.defns$area<=4]<-"DP.A2"
-  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==5]<-"DP.A3"
-  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==6]<-"DP.A4"
-  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==7]<-"DP.A5"
+  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==1]<-"DP.A1"
+  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==2]<-"DP.A2"
+  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==3]<-"DP.A3"
+  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==4]<-"DP.A4"
+  areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==5]<-"DP.A5"
   
   # return fishery ids data frame
   return(list(fishery.quarter=quarter,fishery.areagear=areagear))
--- demo/BET_20002021 - Haikun.R
+++ demo/BET_20002021 - Haikun.R
@@ -1,10 +1,10 @@
 library(BSE)
 # global setup
 raw_data_dir <- "D:/OneDrive - IATTC/IATTC/2022/BSE stuff from Cleridy/spp comp programs_from 2000/Raw data extractions/"
-save_dir <- "D:/OneDrive - IATTC/IATTC/2022/BSE stuff from Cleridy/YFT/"
+save_dir <- "D:/OneDrive - IATTC/IATTC/2022/BSE stuff from Cleridy/BET/"
 yr.start <- 2000
-yr.end <- 2019
-Species <- "YFT"
+yr.end <- 2021
+Species <- "BET"
 grow.increments <- grow.increments.betyftskj # the growth increment matrix
 
 # Get the total unloads for the PS fleet
@@ -17,10 +17,15 @@ lfmm <- read.lfmmdata.f(raw_data_dir,"LengthMM2000-2021.txt")
 lfgrpd <- read.lengthfreq.f(raw_data_dir,"LengthFreq2000-2021.txt")
 
 
-# Running YFT OBJ
+# Running BET OBJ
 PS <- "OBJ"
 
-area.substitution.mat <- area.substitution.mat.YFT.OBJ.2019ExtRev # for OBJ
+area.substitution.mat <- matrix(c(1,3,2,4,5,
+                                  2,4,1,3,5,
+                                  3,1,4,2,5,
+                                  4,5,2,3,1,
+                                  5,4,2,3,1),
+                                ncol = 5, byrow = TRUE) # for OBJ
 
 cae.stratflg <- create.strat.flg.f(cae$latc5,cae$lonc5,is.lwrght=F,cae$month,cae$setype,cae$class,PS=PS,Species=Species)
 check.strat.flg.f(cae$latc5,cae$lonc5,cae.stratflg)
@@ -44,33 +49,38 @@ for(year in yr.start:yr.end) {
   
   assign(paste0("fishery.estimates.", year), fishery.estimates, pos=1)
 }
-save(list=objects(pat="fishery.estimates"),file=paste0(save_dir,"YFT_",PS,"_2000-2021.RData"))
+save(list=objects(pat="fishery.estimates"),file=paste0(save_dir,"BET_",PS,"_2000-2021.new.RData"))
 
 # get final catch and comp output for the stock assessment
-YFT.OBJ.Catch.20002021<-compile.catch.output.f(yr.start,yr.end,PS=PS,Species=Species,c("A1","A2","A3","A4","A5"))
-YFT.OBJ.Comp.20002021<-compile.sizecomps.output.f(yr.start,yr.end,PS=PS,Species=Species)
+BET.OBJ.Catch.20002021<-compile.catch.output.f(yr.start,yr.end,PS=PS,Species=Species,c("A1","A2","A3","A4","A5"))
+BET.OBJ.Comp.20002021<-compile.sizecomps.output.f(yr.start,yr.end,PS=PS,Species=Species)
 
 # save results
-write.csv(YFT.OBJ.Catch.20002021,file=paste0(save_dir,"YFT.OBJ.Catch.20002021.csv"),row.names = FALSE)
-write.csv(YFT.OBJ.Comp.20002021,file=paste0(save_dir,"YFT.OBJ.Comp.20002021.csv"),row.names = FALSE)
+write.csv(BET.OBJ.Catch.20002021,file=paste0(save_dir,"BET.OBJ.Catch.20002021.new.csv"),row.names = FALSE)
+write.csv(BET.OBJ.Comp.20002021,file=paste0(save_dir,"BET.OBJ.Comp.20002021.new.csv"),row.names = FALSE)
 
 # compare results
 library(tidyverse)
 
-YFT_OBJ_Catch.SAC12 <- read.csv("D:/OneDrive - IATTC/IATTC/2022/BSE stuff from Cleridy/spp comp programs_from 2000/current_estimates/YFT_FLOATING-OBJECT CATCH_FORMATTED_1975-2019_for SAC 2020.csv")
-YFT.OBJ.Catch.20002021 <- read.csv(paste0(save_dir,"YFT.OBJ.Catch.20002021.csv"))
+BET_OBJ_Catch.SAC12 <- read.csv("D:/OneDrive - IATTC/IATTC/2022/BSE stuff from Cleridy/spp comp programs_from 2000/current_estimates/BET_FLOATING-OBJECT CATCH_FORMATTED_1975-2019_for SAC 2020.csv")
+BET.OBJ.Catch.20002021 <- read.csv(paste0(save_dir,"BET.OBJ.Catch.20002021.csv"))
+BET.OBJ.Catch.20002021.new <- read.csv(paste0(save_dir,"BET.OBJ.Catch.20002021.new.csv"))
 
-YFT_OBJ_Catch.SAC12 <- YFT_OBJ_Catch.SAC12[,2:8] %>% mutate(Source="Cleridy")
-YFT.OBJ.Catch.20002021 <- YFT.OBJ.Catch.20002021 %>% mutate(Source="Haikun")
+BET_OBJ_Catch.SAC12 <- BET_OBJ_Catch.SAC12[,2:8] %>% mutate(Source="Cleridy")
+BET.OBJ.Catch.20002021 <- BET.OBJ.Catch.20002021 %>% mutate(Source="Haikun")
+BET.OBJ.Catch.20002021.new <- BET.OBJ.Catch.20002021.new %>% mutate(Source="Haikun.new")
 
-YFT_OBJ_Catch <- rbind(YFT_OBJ_Catch.SAC12,YFT.OBJ.Catch.20002021) %>% 
+BET_OBJ_Catch <- rbind(BET_OBJ_Catch.SAC12,BET.OBJ.Catch.20002021,BET.OBJ.Catch.20002021.new) %>% 
   mutate(Year=year+quarter/4-1/8) %>%
   filter(year>1999,year<2020) %>%
-  gather(3:7,key="Area",value="Catch")
+  mutate(EPO=A1+A2+A3+A4+A5) %>%
+  gather(c(3:7,10),key="Area",value="Catch") %>%
+  group_by(year,Area,Source) %>%
+  mutate(Catch_annual=sum(Catch))
 
-ggplot(data=YFT_OBJ_Catch) +
-  geom_line(aes(x=Year,y=Catch,color=Source)) +
-  facet_wrap(~Area,nrow=5) +
+ggplot(data=BET_OBJ_Catch) +
+  geom_line(aes(x=year,y=Catch_annual,color=Source)) +
+  facet_wrap(~Area,nrow=5,scales="free_y") +
   theme_bw()
 
-ggsave(file=paste0(save_dir,"YFT.OBJ.Catch.png"),h=10,w=10)
+ggsave(file=paste0(save_dir,"BET.OBJ.Catch.png"),h=10,w=10)
